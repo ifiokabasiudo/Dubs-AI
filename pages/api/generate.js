01ecf159c7b15b1a30049c5c9fbf7cb136ab779f
Maj_st <68490874+ifiokabasiudo@users.noreply.github.com>
2023-03-12 18:39:06 +0100

    Added pages folder

---
+++ pages/api/generate.js
@@ -0,0 +1,58 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please follow instructions in README.md",
+      }
+    });
+    return;
+  }
+
+  const question = req.body.question || '';
+  if (question.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a question",
+      }
+    });
+    return;
+  }
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "All the mathematical expressions should be written in LaTeX format and configure all inline math to be suitable for MathJax, Images shouldn't be in LaTex format" + question,
+      temperature: 1,
+      max_tokens: 2048,
+      n: 1,
+      presence_penalty: 0.6,
+      // stop: ["\n"],
+      // stream: true,
+    });
+    res.status(200).json({result: completion.data.choices[0].text});
+
+    
+  } catch(error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data); 
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      });
+    }
+  }
+}
+
+
